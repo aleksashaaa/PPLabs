b3bc1052b460ed254dbd5f2c9907a483697b15f1
Alexandra <sabalinaaleksandra36@gmail.com>
2024-02-16 09:55:04 +0600

    LAB5

--- Lab5/Regex.py
+++ Lab5/Regex.py
@@ -22,8 +22,8 @@ print("Num 3 -", re.findall(r'\b[a-z]+_[a-z]+\b', s))
 print("Num 3 -", re.findall(r'\b[a-z]+_[a-z]+\b', s2))
 
 #4
-print("Num 4 -", re.findall(r'[A-Z][a-z]', s))
-print("Num 4 -", re.findall(r'[A-Z][a-z]', s2))
+print("Num 4 -", re.findall(r'[A-Z][a-z]+', s))
+print("Num 4 -", re.findall(r'[A-Z][a-z]+', s2))
 
 #5
 print("Num 5 -", re.findall(r'a.*b', s))
@@ -33,19 +33,19 @@ print("Num 5 -", re.findall(r'a.*b', s2))
 #print("Num 6 -", re.sub(r'[ ,.]', "|", s))
 print("Num 6 -", re.sub(r'[ ,.]', "|", s2))
 
-#7 -
-#print("Num 7 -", re.sub(r'_([a-z])', lambda m: m.group(1).upper(), s))
-print("Num 7 -", re.sub(r'_([a-z])', lambda m: m.group(1).upper(), s2))
+#7 
+#print("Num 7 -", re.sub(r'_([a-zA-z])', lambda m: m.group(1).upper(), s))
+print("Num 7 -", re.sub(r'_([a-zA-z])', lambda m: m.group(1).upper(), s2))
 
 #8
-#print("Num 8 -", re.split("[A-Z]", s))
-print("Num 8 -", re.split("[A-Z]", s2))
+#print("Num 8 -", re.split(r"[A-Z]", s))
+print("Num 8 -", re.split(r"[A-Z]", s2))
 
-#9 --
-#print("Num 9 -", re.sub(r'([a-z])([A-Z])', r'\1 \2', s)))
-print("Num 9 -", re.sub(r'([a-z])([A-Z])', r'\1 \2', s2))
+#9 
+#print("Num 9 -", re.sub(r"([A-Z])", " \\1", s))
+print("Num 9 -", re.sub(r"([A-Z])", " \\1", s2))
 
 #10 ---
-#print("Num 10 -", , s))
-#print("Num 10 -", //, s2))
+#print("Num 10 -", re.sub(r"([a-z])([A-Z])", "\\1_\\2", s).lower())
+print("Num 10 -", re.sub(r"([a-z])([A-Z])", "\\1_\\2", s2).lower())
 f.close()
